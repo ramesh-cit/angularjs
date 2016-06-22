d4fa1e6e404f49f8df40715cf41e97a03cf4364e
ramesh-cit <ramesh.s@whitelotus.co.in>
2016-06-22 13:29:18 -0400

    Commit from Windows 10 Using SourceTree

--- HelloAngular/app/root.js
+++ HelloAngular/app/root.js
@@ -1,4 +1,19 @@
-angular.module('root', [])
-    .controller("index", ["$scope", function ($scope) {
-        $scope.message = "Hello World!";
+(function() {
+
+var app = angular.module('root', []);
+app.controller("index", ["$scope", function ($scope) {
+        $scope.message = "Welcome to Angular JS!";
     }]);
+ 
+app.controller('StoreController', function() {
+	this.product=gem;	
+});
+
+var gem = {
+	name: "Diamond",
+	price :1278.67,
+	description : "Made in India"
+}
+
+})(); 
+
